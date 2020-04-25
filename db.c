feade5cb00333742fc9b8df29fb341a619ee4e01
Stefan R. Sathianathen <stefansathianathen@gmail.com>
2020-04-25 20:04:08 +1000

    use strncpy instead of memcpy to only copy over initialized bytes

--- db.c
+++ db.c
@@ -251,8 +251,8 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
 
 void serialize_row(Row* source, void* destination) {
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 //==============================================================================
