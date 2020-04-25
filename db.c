077a26cbc6615daa24a833b8c2d90eaf1656fdb1
Stefan R. Sathianathen <stefansathianathen@gmail.com>
2020-04-25 16:57:06 +1000

    recognize insert or select

--- db.c
+++ db.c
@@ -12,6 +12,24 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
 //==============================================================================
 // Function declartions 
 
@@ -19,7 +37,10 @@ InputBuffer* new_input_buffer();
 void print_prompt();
 void read_input(InputBuffer* input_buffer);
 void close_input_buffer(InputBuffer* input_buffer);
-
+MetaCommandResult do_meta_command(InputBuffer* input_buffer);
+PrepareResult prepare_statement(InputBuffer* input_buffer,
+                                Statement* statement);
+void execute_statement(Statement* statement);                                
 //==============================================================================
 // Main
 
@@ -29,12 +50,28 @@ int main(int argc, char* argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit") == 0){
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else{
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case(META_COMMAND_SUCCESS):
+                    continue;
+                case(META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+               }
         }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed. \n");
+        
     }
 }
 
@@ -79,4 +116,46 @@ void read_input(InputBuffer* input_buffer){
 void close_input_buffer(InputBuffer* input_buffer){
     free(input_buffer->buffer);
     free(input_buffer);
+}
+
+//==============================================================================
+// Figure out what to do with command
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if (strcmp(input_buffer->buffer, ".exit") == 0){
+        exit(EXIT_SUCCESS);
+    } else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+//==============================================================================
+// Prepare statement
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,
+                                Statement* statement) {
+                                    if (strncmp(input_buffer->buffer, "insert", 6) == 0){
+                                        statement->type = STATEMENT_INSERT;
+                                        return PREPARE_SUCCESS;
+                                    }
+                                    if (strcmp(input_buffer->buffer, "select") == 0){
+                                        statement->type = STATEMENT_SELECT;
+                                        return PREPARE_SUCCESS;
+                                    }
+
+                                    return PREPARE_UNRECOGNIZED_STATEMENT;
+                                }
+
+//==============================================================================
+// Execute SQL statement
+
+void execute_statement(Statement* statement){
+    switch (statement->type){
+        case (STATEMENT_INSERT):
+          printf("insert\n");
+          break;
+        case (STATEMENT_SELECT):
+          printf("select\n");
+          break;
+    }
 }
